ef2dfc3220adcfff5a9b3af6fada0b85550d2b8c
eMedx <40034674+JohnAPA@users.noreply.github.com>
2020-10-21 12:05:05 -0400

    Create commander.sh

---
+++ applications/commander/commander.sh
@@ -0,0 +1,51 @@
+#
+# Commander File takes input you place in it, and executes it by using the alias $ 'commander'. It will save a copy of it in another file, but clears this one out 
+#     after the execution. The intention of this file is for bulk movement of files. 
+# ------------------------------------------
+#==========================
+# Last Command
+#---------------
+#
+#
+#
+#
+#==========================
+# New Command: 
+
+#--BEGIN
+
+
+
+
+echo "module.exports = {
+
+   links: {
+      maestro: "http://www.maestro.com",
+      amion_original: "http://amion.com",
+      amion_new: "https://amion.empa.app"
+   },
+
+   data: {
+      deptDir: {
+         link: "https://amion.empa.app/deptdir",
+         fs:   "/data/deptDir.json"
+      }
+
+   },
+
+   config: {
+      template: "UI_KIT"
+   }
+
+}">>/admin/amion/config/config.amion.js
+
+
+
+
+
+
+
+
+
+
+#--END
